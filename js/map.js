a7a4a857618cd166a286657ba00720d4341f9b63
Gordievskaya.Y <Gordievskaya.Y@WB-2796>
2018-11-23 17:57:52 +0300

    Продолжение работы с объектом оффера

--- js/map.js
+++ js/map.js
@@ -1,20 +1,22 @@
 'use strict';
 
-var TITLES = ['Большая уютная квартира', 'Маленькая неуютная квартира', 'Огромный прекрасный дворец',
-'Маленький ужасный дворец', 'Красивый гостевой домик', 'Некрасивый негостеприимный домик', 'Уютное бунгало далеко от моря',
- 'Неуютное бунгало по колено в воде'];
- var MIN_PRICE = 1000;
- var MAX_PRICE = 1000000;
- var MIN_AVATAR_SRC = 1;
- var MAX_AVATAR_SRC = 8;
- var MIN_COUNT_ROOMS = 1;
- var MAX_COUNT_ROOMS = 5;
- var TYPES_HOUSES = ['palace', 'flat', 'house', 'bungalo'];
- var CHECKIN_TIME = ['12:00', '13:00', '14:00'];
- var CHECKOUT_TIME = ['12:00', '13:00', '14:00'];
- 
- var map = document.querySelector('.map');
- map.classList.remove('map--faded');
+var TITLES = ['Большая уютная квартира', 'Маленькая неуютная квартира', 'Огромный прекрасный дворец', 'Маленький ужасный дворец', 'Красивый гостевой домик', 'Некрасивый негостеприимный домик', 'Уютное бунгало далеко от моря', 'Неуютное бунгало по колено в воде'];
+var MIN_PRICE = 1000;
+var MAX_PRICE = 1000000;
+var MIN_AVATAR_SRC = 1;
+var MAX_AVATAR_SRC = 8;
+var MIN_COUNT_ROOMS = 1;
+var MAX_COUNT_ROOMS = 5;
+var MAX_COUNT_GUESTS = 100;
+var TYPES_HOUSES = ['palace', 'flat', 'house', 'bungalo'];
+var FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
+var PICTURES = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg'];
+var CHECKIN_TIME = ['12:00', '13:00', '14:00'];
+var CHECKOUT_TIME = ['12:00', '13:00', '14:00'];
+var MIN_LOCATION_Y = 130;
+var MAX_LOCATION_Y = 630;
+var map = document.querySelector('.map');
+map.classList.remove('map--faded');
 
 // Функция для вычисления уникального случайного числа
 var getUniqNumber = function (min, max) {
@@ -32,9 +34,16 @@ var getUniqNumber = function (min, max) {
   return outputArr;
 };
 
-//рандомное значение
+// рандомное значение
 var getRandom = function (min, max) {
   return Math.round(Math.random() * (max - min) + min);
+};
+
+// получаем рандомный массив рандомной длины
+var getRandomString = function (arr) {
+  var randomFrom = getRandom(0, arr.length - 1);
+  var randomTo = getRandom(0, arr.length - 1);
+  arr.splice(randomFrom, randomTo);
 };
 
 var offers = [
@@ -45,15 +54,21 @@ var offers = [
 
     "offer": {
       "title": TITLES[getUniqNumber(0, TITLES.length - 1)[0]], // для первого элемента
-    //  "address": ,//
+      "address": this.location.x,
       "price": getRandom(MIN_PRICE, MAX_PRICE),
       "type": TYPES_HOUSES[getRandom(0, TYPES_HOUSES.length - 1)],
       "rooms": getRandom(MIN_COUNT_ROOMS, MAX_COUNT_ROOMS),
-      //"guests" какой интервал ?
+      "guests": getRandom(0, MAX_COUNT_GUESTS),
       "checkin": CHECKIN_TIME[getRandom(0, CHECKIN_TIME.length)],
       "checkout": CHECKOUT_TIME[getRandom(0, CHECKOUT_TIME.length)],
-      "features": '',
-      "description": ''
+      "features": getRandomString(FEATURES),
+      "description": '',
+      "photos": PICTURES[getUniqNumber(0, PICTURES.length)]
+    },
+
+    "location": {
+      "x": '',
+      "y": getRandom(MIN_LOCATION_Y, MAX_LOCATION_Y)
     }
   }
 ];
