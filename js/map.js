21298b58278e39b75d2f576824f30ed96c0e1275
Julyme3 <jg0311@bk.ru>
2018-11-24 13:31:34 +0300

    Обект с данными для объявлений

--- js/map.js
+++ js/map.js
@@ -1,21 +1,32 @@
 'use strict';
 
-var TITLES = ['Большая уютная квартира', 'Маленькая неуютная квартира', 'Огромный прекрасный дворец', 'Маленький ужасный дворец', 'Красивый гостевой домик', 'Некрасивый негостеприимный домик', 'Уютное бунгало далеко от моря', 'Неуютное бунгало по колено в воде'];
-var MIN_PRICE = 1000;
-var MAX_PRICE = 1000000;
-var MIN_AVATAR_SRC = 1;
-var MAX_AVATAR_SRC = 8;
-var MIN_COUNT_ROOMS = 1;
-var MAX_COUNT_ROOMS = 5;
-var MAX_COUNT_GUESTS = 100;
-var TYPES_HOUSES = ['palace', 'flat', 'house', 'bungalo'];
-var FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
-var PICTURES = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg'];
-var CHECKIN_TIME = ['12:00', '13:00', '14:00'];
-var CHECKOUT_TIME = ['12:00', '13:00', '14:00'];
-var MIN_LOCATION_Y = 130;
-var MAX_LOCATION_Y = 630;
+var NoticeData = {
+  TITLES: ['Большая уютная квартира', 'Маленькая неуютная квартира', 'Огромный прекрасный дворец', 'Маленький ужасный дворец', 'Красивый гостевой домик', 'Некрасивый негостеприимный домик', 'Уютное бунгало далеко от моря', 'Неуютное бунгало по колено в воде'],
+  MIN_PRICE: 1000,
+  MAX_PRICE: 1000000,
+  MIN_AVATAR_SRC: 1,
+  MAX_AVATAR_SRC: 8,
+  MIN_COUNT_ROOMS: 1,
+  MAX_COUNT_ROOMS: 5,
+  MAX_COUNT_GUESTS: 100,
+  TYPES_HOUSES: ['palace', 'flat', 'house', 'bungalo'],
+  FEATURES: ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'],
+  PICTURES: ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg'],
+  CHECKIN_TIME: ['12:00', '13:00', '14:00'],
+  CHECKOUT_TIME: ['12:00', '13:00', '14:00'],
+  COORDINATE_X: 1000,
+  COORDINATE_Y: 500
+};
+
+var Pin = {
+  MIN_LOCATION_Y: 130,
+  MAX_LOCATION_Y: 630
+};
+
+var COUNT_NOTICES = 8;
 var map = document.querySelector('.map');
+var widthMap = map.offsetWidth;
+var pinTemplate = document.querySelector('#pin').content.querySelector('.map__pin');
 map.classList.remove('map--faded');
 
 // Функция для вычисления уникального случайного числа
@@ -23,8 +34,8 @@ var getUniqNumber = function (min, max) {
   var uniqElement; // рандомное значение
   var outputArr = []; // массив из неповторяющихся значений
 
-  while (outputArr.length < max) {
-    uniqElement = (Math.round(Math.random() * (max - min) + min));
+  while (outputArr.length <= max - min) {
+    uniqElement = Math.round(Math.random() * (max - min) + min);
 
     if (outputArr.indexOf(uniqElement) === -1) {
       outputArr.push(uniqElement);
@@ -41,34 +52,64 @@ var getRandom = function (min, max) {
 
 // получаем рандомный массив рандомной длины
 var getRandomString = function (arr) {
-  var randomFrom = getRandom(0, arr.length - 1);
-  var randomTo = getRandom(0, arr.length - 1);
+  var randomFrom = getRandom(0, arr.length - 1); // с какого индекса
+  var randomTo = getRandom(0, arr.length - 1); // какое количество
   arr.splice(randomFrom, randomTo);
+  return arr;
+};
+// получаем рандомный массив с урлами из PICTURES
+var concateUniqString = function (inputArr) {
+  var outputArr = [];
+  var arrUniqElements = getUniqNumber(0, inputArr.length - 1); // массив неповторяющихся элементов
+
+  for (var i = 0; i <= arrUniqElements.length - 1; i++) {
+    outputArr.push(inputArr[arrUniqElements[i]]); // заполняем массив конкретными значениями
+  }
+
+  return outputArr;
 };
 
-var offers = [
-  {
-    "author": {
-      "avatar": 'img/avatars/user0' + getUniqNumber(MIN_AVATAR_SRC, MAX_AVATAR_SRC) + '.png'
-    },
+// генерируем массив данных объявлений
+var renderDataNotice = function () {
+  var notice;
+  var noticesArr = []; // массив с объявлениями
+  var arrTitles = getUniqNumber(0, NoticeData.TITLES.length - 1); // массив неповторяющихся значений
+  var arrImgSrc = getUniqNumber(NoticeData.MIN_AVATAR_SRC, NoticeData.MAX_AVATAR_SRC); // хвост для формирования пути к картинке
 
-    "offer": {
-      "title": TITLES[getUniqNumber(0, TITLES.length - 1)[0]], // для первого элемента
-      "address": this.location.x,
-      "price": getRandom(MIN_PRICE, MAX_PRICE),
-      "type": TYPES_HOUSES[getRandom(0, TYPES_HOUSES.length - 1)],
-      "rooms": getRandom(MIN_COUNT_ROOMS, MAX_COUNT_ROOMS),
-      "guests": getRandom(0, MAX_COUNT_GUESTS),
-      "checkin": CHECKIN_TIME[getRandom(0, CHECKIN_TIME.length)],
-      "checkout": CHECKOUT_TIME[getRandom(0, CHECKOUT_TIME.length)],
-      "features": getRandomString(FEATURES),
-      "description": '',
-      "photos": PICTURES[getUniqNumber(0, PICTURES.length)]
-    },
+  for (var i = 0; i < COUNT_NOTICES; i++) {
+    notice =
+      {
+        'author': {
+          'avatar': 'img/avatars/user0' + arrImgSrc[i] + '.png'
+        },
 
-    "location": {
-      "x": '',
-      "y": getRandom(MIN_LOCATION_Y, MAX_LOCATION_Y)
-    }
+        'offer': {
+          'title': NoticeData.TITLES[arrTitles[i]], // заголовок
+          'address': getRandom(0, NoticeData.COORDINATE_X) + ', ' + getRandom(0, NoticeData.COORDINATE_Y),
+          'price': getRandom(NoticeData.MIN_PRICE, NoticeData.MAX_PRICE),
+          'type': NoticeData.TYPES_HOUSES[getRandom(0, NoticeData.TYPES_HOUSES.length - 1)],
+          'rooms': getRandom(NoticeData.MIN_COUNT_ROOMS, NoticeData.MAX_COUNT_ROOMS),
+          'guests': getRandom(0, NoticeData.MAX_COUNT_GUESTS),
+          'checkin': NoticeData.CHECKIN_TIME[getRandom(0, NoticeData.CHECKIN_TIME.length - 1)],
+          'checkout': NoticeData.CHECKOUT_TIME[getRandom(0, NoticeData.CHECKOUT_TIME.length - 1)],
+          'features': getRandomString(NoticeData.FEATURES),
+          'description': '',
+          'photos': concateUniqString(NoticeData.PICTURES)
+        },
+
+        'location': {
+          'x': getRandom(0, widthMap),
+          'y': getRandom(Pin.MIN_LOCATION_Y, Pin.MAX_LOCATION_Y)
+        }
+      };
+    noticesArr.push(notice);
   }
-];
+};
+renderDataNotice();
+
+var renderPin = function () {
+  var pinElement = pinTemplate.cloneNode(true);
+  document.querySelector('.map__pins').appendChild(pinElement);
+
+};
+renderPin();
